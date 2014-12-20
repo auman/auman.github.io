69764633e99af1f85b0b27f77896d9829be23611
Dmitry Velikanov <nothing@example.com>
2014-12-20 17:15:18 +0600

    generic update new proofs

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git add .  && python ./gen.py > ./index.html && git commit -m 'generic update new proofs' -a && git push origin master
+python ./gen.py > ./index.html && git add . && git commit -m 'generic update new proofs' -a && git push origin master
 
