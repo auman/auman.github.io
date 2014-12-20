231c984db3e9c5d9de3477b306399e0366257d52
Dmitry Velikanov <nothing@example.com>
2014-12-20 12:59:45 +0600

    count of proofs

--- gen.py
+++ gen.py
@@ -13,11 +13,13 @@ tpl = """
 <style>
 body {{background-color:#dddddd;}}
 h1 {{color:#FF6600;}}
+h2 {{text-align:center;}}
 </style>
 </head>
 
 <body>
 <h1 style="text-align:center;">Предновогодний пруфтред 2014-2015</h1>
+<h2>Всего пруфов: {}</h2>
 <div style="width:900px;margin:0px auto;">
 {}
 </div>
@@ -33,8 +35,10 @@ def main():
 
     files = os.listdir('i')
     cols = ['',] * COLS_COUNT
+    total = 0
     for col, f in zip(cycle(range(COLS_COUNT)),
             sorted(files, key=lambda x: int(x[:-4]))):
+        total += 1
         im = Image.open(os.path.join('i', f))
         im.thumbnail([160, 160])
         thumb_path = os.path.join('thumbs', ntpath.basename(f))
@@ -43,7 +47,7 @@ def main():
 
     data = '</div><div style="float:left;width:180px;">'.join(cols)
     data = '<div style="float:left;width:180px;">{}</div>'.format(data)
-    print(tpl.format(data))
+    print(tpl.format(total, data))
 
 if __name__ == '__main__':
     main()
