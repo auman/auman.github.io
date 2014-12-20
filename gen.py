784d5822b6cf047d5a20ccbfb89aee1ae9730d16
Dmitry Velikanov <nothing@example.com>
2014-12-20 12:33:38 +0600

    html improvement to look better

--- gen.py
+++ gen.py
@@ -1,13 +1,15 @@
 
 import os
 import ntpath
+from itertools import cycle
 from PIL import Image
 
 tpl = """
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
 <title>Предновогодний пруфтред 2014-2015</title>
+<meta charset="UTF-8">
 </head>
 
 <body>
@@ -21,17 +23,22 @@ tpl = """
 
 """
 
+COLS_COUNT = 5
+
 def main():
 
     files = os.listdir('i')
-    data = ''
-    for f in sorted(files, key=lambda x: int(x[:-4])):
+    cols = ['',] * COLS_COUNT
+    for col, f in zip(cycle(range(COLS_COUNT)),
+            sorted(files, key=lambda x: int(x[:-4]))):
         im = Image.open(os.path.join('i', f))
         im.thumbnail([160, 160])
         thumb_path = os.path.join('thumbs', ntpath.basename(f))
         im.save(thumb_path, 'JPEG', quality=80, optimize=True, progressive=True)
-        data += '<div style="float: left;margin: 6px;"><a href="./i/{}" target="_blank"><img src="./thumbs/{}" width="160px"></div>'.format(f, f)
+        cols[col] += '<a href="./i/{}" style="display:block;margin:6px;" target="_blank"><img src="./thumbs/{}" width="160px"/>'.format(f, f)
 
+    data = '</div><div style="float:left;width:180px;">'.join(cols)
+    data = '<div style="float:left;width:180px;">{}</div>'.format(data)
     print(tpl.format(data))
 
 if __name__ == '__main__':
